f3cf3ab6d482be6b25eca15f0cb201d9b6a1caef
coderx31 <shenal.fernando10@gmail.com>
2021-02-16 20:26:39 +0530

    done with starter page and create All books activity

--- app/src/main/java/com/coderx/mylibrary/MainActivity.java
+++ app/src/main/java/com/coderx/mylibrary/MainActivity.java
@@ -3,12 +3,28 @@ package com.coderx.mylibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnWantToRead, btnCurrentlyReading, btnFavorite, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*initialize all the views*/
+        initViews();
+    }
+
+    private void initViews(){
+        /* all the views */
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWantToRead = findViewById(R.id.btnWantToRead);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnFavorite = findViewById(R.id.btnFavorite);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
