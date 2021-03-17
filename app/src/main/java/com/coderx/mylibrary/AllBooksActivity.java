4c910fefe504e5bdbf5dbaf9a50758fc66b4accc
coderx31 <shenal.fernando10@gmail.com>
2021-03-17 20:33:18 +0530

    created the recycler view

--- app/src/main/java/com/coderx/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/coderx/mylibrary/AllBooksActivity.java
@@ -1,14 +1,31 @@
 package com.coderx.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, "1Q84","Haruki Murakami",1350,"http://jasonlefkowitz.net/wp-content/uploads/2015/06/1q84-660x1024.jpg",
+                "A work of maddeling brilliance","Long Description"));
+        adapter.setBooks(books);
     }
 }
---
+++ app/src/main/java/com/coderx/mylibrary/Book.java
@@ -0,0 +1,90 @@
+package com.coderx.mylibrary;
+
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private int pages;
+    private String imgURL;
+    private String shortDesc;
+    private String longDesc;
+
+    public Book(int id, String name, String author, int pages, String imgURL, String shortDesc, String longDesc) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.imgURL = imgURL;
+        this.shortDesc = shortDesc;
+        this.longDesc = longDesc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public String getLongDesc() {
+        return longDesc;
+    }
+
+    public void setLongDesc(String longDesc) {
+        this.longDesc = longDesc;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", pages=" + pages +
+                ", imgURL='" + imgURL + '\'' +
+                ", shortDesc='" + shortDesc + '\'' +
+                ", longDesc='" + longDesc + '\'' +
+                '}';
+    }
+}
